dd66c80f9579acbacd2712995d6963e81079cb60
Angel Montenegro <angel.montenegro.jimenez@gmail.com>
2015-05-14 16:11:24 -0600

    Exchanging code

--- orcid-api-walkthrough/WebContent/js/app.js
+++ orcid-api-walkthrough/WebContent/js/app.js
@@ -1,38 +1,32 @@
-var HackatonApp = angular.module('HackatonApp', ['ngRoute','OauthController']);
+var HackatonApp = angular.module('HackatonApp',
+		[ 'ngRoute', 'OauthController' ]);
 
-HackatonApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $routeProvider.
-  when('/', {
-	    templateUrl: 'partials/introduction.html',
-	    controller: 'IntroductionController'
-	  }).
-  when('/description', {
-    templateUrl: 'partials/description.html',
-    controller: 'DescriptionController'
-  }).
-  when('/technologies', {
-    templateUrl: 'partials/technologies.html',
-    controller: 'TechnologiesController'
-  }).
-  when('/get_code', {
-    templateUrl: 'partials/get_code.html',
-    controller: 'GetPermissionsController'
-  }).
-  when('/get_token', {
-    templateUrl: 'partials/get_token.html',
-    controller: 'GetTokenController'
-  }).
-  when('/read_profile', {
-    templateUrl: 'partials/read_record.html',
-    controller: 'ReadRecordController'
-  }).
-  when('/add_work', {
-    templateUrl: 'partials/add_work.html',
-    controller: 'AddWorkController'
-  }).
-  otherwise({	  
-    redirectTo: '/'
-  });
-  
-  $locationProvider.html5Mode(true);
-}]);
+HackatonApp.config([ '$routeProvider', '$locationProvider',
+		function($routeProvider, $locationProvider) {
+			$routeProvider.when('/', {
+				templateUrl : 'partials/introduction.html',
+				controller : 'IntroductionController'
+			}).when('/description', {
+				templateUrl : 'partials/description.html',
+				controller : 'DescriptionController'
+			}).when('/technologies', {
+				templateUrl : 'partials/technologies.html',
+				controller : 'TechnologiesController'
+			}).when('/get_code', {
+				templateUrl : 'partials/get_code.html',
+				controller : 'GetPermissionsController'
+			}).when('/get_token', {
+				templateUrl : 'partials/get_token.html',
+				controller : 'GetTokenController'
+			}).when('/read_profile', {
+				templateUrl : 'partials/read_record.html',
+				controller : 'ReadRecordController'
+			}).when('/add_work', {
+				templateUrl : 'partials/add_work.html',
+				controller : 'AddWorkController'
+			}).otherwise({
+				redirectTo : '/'
+			});
+
+			$locationProvider.html5Mode(true);
+		} ]);
--- orcid-api-walkthrough/WebContent/js/controllers.js
+++ orcid-api-walkthrough/WebContent/js/controllers.js
@@ -1,14 +1,15 @@
-var OauthController = angular.module("OauthController", ['ngCookies']);
+var OauthController = angular.module("OauthController", [ 'ngCookies' ]);
 
 OauthController.controller("IntroductionController", [ '$scope', '$http',
-		'$routeParams', '$location', function($scope, $http, $routeParams, $location) {
+		'$routeParams', '$location',
+		function($scope, $http, $routeParams, $location) {
 			var code = $location.search()['code'];
-			
-			if(code === undefined || code === null) {
+
+			if (code === undefined || code === null) {
 				$location.path("/");
 			} else {
-				$location.path( "/get_token" );	            		
-			}			
+				$location.path("/get_token");
+			}
 		} ]);
 
 OauthController.controller("DescriptionController", [ '$scope', '$http',
@@ -21,50 +22,92 @@ OauthController.controller("TechnologiesController", [ '$scope', '$http',
 			console.log("In Technologies controller");
 		} ]);
 
-OauthController.controller("GetPermissionsController", [ '$scope', '$http', '$cookies',
-		function GetCodeController($scope, $http, $cookies) {
-			$scope.authString = "http://[api_url]//oauth/authorize?client_id=[client_id]&response_type=code&redirect_uri=[redirect_uri]&scope=[scope]";
-			$scope.load = function() {
-				console.log("In get code controller");
-			}, 
-			$scope.getCode = function() {
-				//Build the string
-				var apiUri = $scope.authString;
-				apiUri = apiUri.replace('[api_url]', 'qa.orcid.org');
-				apiUri = apiUri.replace('[client_id]', $scope.form.client_id);
-				apiUri = apiUri.replace('[redirect_uri]', 'http://localhost:8080/orcid-api-walkthrough/');
-				apiUri = apiUri.replace('[scope]', $scope.form.scope);
-				console.log(apiUri);
-				
-				//Save values in cookies so we can use them later
-				$cookies.put('orcid_oauth2_client_id', $scope.form.client_id);
-				$cookies.put('orcid_oauth2_redirect_uri', 'http://localhost:8080/orcid-api-walkthrough/');
-								
-				setTimeout(function() {
-					window.location.href = apiUri;
-	            }, 125);				
-			};
-			
-			$scope.load();
-		} ]);
+OauthController
+		.controller(
+				"GetPermissionsController",
+				[
+						'$scope',
+						'$http',
+						'$cookies',
+						function GetCodeController($scope, $http, $cookies) {
+							$scope.authString = "http://[api_url]//oauth/authorize?client_id=[client_id]&response_type=code&redirect_uri=[redirect_uri]&scope=[scope]";
+									$scope.load = function() {
+										console.log("In get code controller");
+									},
+									$scope.getCode = function() {
+										// Build the string
+										var apiUri = $scope.authString;
+										apiUri = apiUri.replace('[api_url]',
+												'qa.orcid.org');
+										apiUri = apiUri.replace('[client_id]',
+												$scope.form.client_id);
+										apiUri = apiUri
+												.replace('[redirect_uri]',
+														'http://localhost:8080/orcid-api-walkthrough/');
+										apiUri = apiUri.replace('[scope]',
+												$scope.form.scope);
+										console.log(apiUri);
 
+										// Save values in cookies so we can use
+										// them later
+										$cookies.put('orcid_oauth2_client_id',
+												$scope.form.client_id);
+										$cookies
+												.put(
+														'orcid_oauth2_redirect_uri',
+														'http://localhost:8080/orcid-api-walkthrough/');
 
-OauthController.controller("GetTokenController", [ '$scope', '$http', '$location', '$cookies', 
-                                                   function($scope, $http, $location, $cookies) {
-			$scope.getCode = function() {
-				console.log("In get token controller");
-				var code = $location.search()['code'];
-				var cookie1 = $cookies.get('orcid_oauth2_client_id');
-				console.log(cookie1);
-				if (code === undefined || code === null) {
-					console.log("Code not specified");
-				} else {
-					console.log(code);
-				}
-			};
-			
-			$scope.getCode();
-		} ]);
+										setTimeout(function() {
+											window.location.href = apiUri;
+										}, 125);
+									};
+
+							$scope.load();
+						} ]);
+
+OauthController
+		.controller(
+				"GetTokenController",
+				[
+						'$scope',
+						'$http',
+						'$location',
+						'$cookies',
+						function($scope, $http, $location, $cookies) {
+
+							$scope.access_code = null;
+							$scope.client_id = null;
+							
+							$scope.getCode = function() {
+								$scope.access_code = $location.search()['code'];
+								$scope.client_id = $cookies.get('orcid_oauth2_client_id');																			
+								console.log($scope.access_code);
+								console.log($scope.client_id);
+								// If the code is not specified, return the view
+								// to the root view
+								if ($scope.access_code === undefined || $scope.access_code === null) {
+									$location.path("/");
+								}
+							};
+
+							$scope.exchangeCode = function() {																
+								$.ajax({
+							        url: 'http://pub.qa.orcid.org/oauth/token',
+							        type: 'GET',
+							        data: { client_id: $scope.client_id, client_secret : $scope.client_secret, grant_type : authorization_code, redirect_uri : 'http://localhost:8080/orcid-api-walkthrough/', code : $scope.access_code} ,
+							        contentType: 'application/json; charset=utf-8',
+							        success: function (response) {
+							            console.log("this is the response"); 
+							            console.log(angular.toJson(response));
+							        },
+							        error: function () {
+							        	console.log("ERROR!!!!");							            
+							        }
+							    }); 																								
+							};
+
+							$scope.getCode();
+						} ]);
 
 OauthController.controller("ReadRecordController", [ '$scope', '$http',
 		'$routeParams', function($scope, $http, $routeParams) {
